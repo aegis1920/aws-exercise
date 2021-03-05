bdc8be9aa8873c20a32e8efc7d68968e2c905c9e
aegis <aegis1920@gmail.com>
2021-03-05 21:41:38 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/bingbong/awsexercise/AwsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.bingbong.awsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bingbong/awsexercise/AwsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingbong.awsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
