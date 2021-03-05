6a3a76acd344f2b77d4aa2c9c671040a784ea8e0
aegis <aegis1920@gmail.com>
2021-03-05 21:51:35 +0900

    feat : 초간단 컨트롤러 작성

---
+++ src/main/java/com/bingbong/awsexercise/controller/AwsExerciseController.java
@@ -0,0 +1,20 @@
+package com.bingbong.awsexercise.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AwsExerciseController {
+
+    @GetMapping
+    public String projectString() {
+        return "AWS Exercise의 bingbong 프로젝트입니다.";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok()
+            .build();
+    }
+}
