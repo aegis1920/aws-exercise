58cbbf9bc74d847ac3b2cfc1d4873768455fbcb5
aegis <aegis1920@gmail.com>
2021-03-21 16:28:30 +0900

    fix : validate server

--- scripts/validate_server.sh
+++ scripts/validate_server.sh
@@ -2,12 +2,27 @@ echo '======================='
 echo 'Running validate_server'
 echo '======================='
 
-result=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/health)
+for retry_count in {1..10}
+do
+  response=$(curl -s http://localhost/health)
+  ok_count=$(echo "$response" | grep 'ok' | wc -l)
 
-echo $result
+  if [ "$ok_count" -ge 1 ]
+  then
+      echo "> Health check 성공"
+      break
+  else
+      echo "> Health check의 응답을 알 수 없거나 혹은 status가 OK가 아닙니다."
+      echo "> Health check: ${response}"
+  fi
 
-if [[ "$result" =~ "200" ]]; then
-  exit 0
-else
-  exit 1
-fi
+  if [ $retry_count -eq 10 ]
+  then
+    echo "> Health check 실패. "
+    echo "> 배포를 종료합니다."
+    exit 1
+  fi
+
+  echo "> Health check 연결 실패. 재시도..."
+  sleep 10
+done
